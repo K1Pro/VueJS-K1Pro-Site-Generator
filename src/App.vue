67bbcaa132362c98a7dd872add7d44a7a5d39181
K1Pro <support@k1pro.net>
2023-11-03 13:59:49 +0100

    connecting to backend API

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,25 @@ export default {
     // ...Pinia.mapWritableState(useUserStore, ['accessToken', 'sessionID', 'loggedIn', 'userData', 'endPts']),
   },
 
-  methods: {},
+  methods: {
+    async getSite() {
+      try {
+        const response = await fetch(servrURL + this.siteStore.pathname, {
+          method: 'GET',
+        });
+        const getSiteResJSON = await response.json();
+        console.log(getSiteResJSON);
+      } catch (error) {
+        console.log(error.toString());
+      }
+    },
+  },
 
   watch: {},
 
-  created() {},
+  created() {
+    this.getSite();
+  },
 };
 </script>
 
--- src/stores/site.js
+++ src/stores/site.js
@@ -2,7 +2,7 @@ const useSiteStore = Pinia.defineStore('site', {
   state: () => {
     return {
       hostname: window.location.hostname,
-      pathname: window.location.pathname,
+      pathname: window.location.pathname.replaceAll('/node/vuejs/Frontends/Site/', '').replaceAll('/', ''),
     };
   },
   actions: {
