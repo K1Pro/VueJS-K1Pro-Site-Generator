d4bc43a96f92a3d4525b4a9ac644ec3057d62abe
K1Pro <support@k1pro.net>
2023-12-08 16:58:50 +0100

    horizontal menu component added delete and select

--- src/components/Horizontal_menu.vue
+++ src/components/Horizontal_menu.vue
@@ -4,7 +4,9 @@
     :name="elValue.name"
     :class="respToggle ? elKey : elKey + ' responsive'"
     :style="{
-      backgroundColor: this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['backgroundColor'],
+      backgroundColor: this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['backgroundColor']
+        ? this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['backgroundColor']
+        : '#808080',
     }"
   >
     <template v-for="(menuItem, index) in elValue['menu-items']">
@@ -12,36 +14,63 @@
       <template v-else-if="loggedIn"
         ><input
           type="text"
-          name="email"
-          v-model="this.site.params.htmlElements[elIndex]['horizontal-menu']['menu-items'][index]" />
-        <template v-if="index === elValue['menu-items'].length - 1"
-          ><span>Text Color:</span
-          ><input
-            type="color"
-            name="horizontal_menuColor"
-            v-model="this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['color']" /><span
-            >Background Color:</span
-          ><input
-            type="color"
-            name="horizontal_menuBackgroundColor"
-            v-model="this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['backgroundColor']" /></template
-      ></template>
+          :name="menuItem"
+          v-model="this.site.params.htmlElements[elIndex]['horizontal-menu']['menu-items'][index]"
+          :style="{
+            width: this.site.params.htmlElements[elIndex]['horizontal-menu']['menu-items'][index].length * 9 + 'px',
+          }"
+        />
+        <template v-if="index > 1">
+          <button class="fa-solid fa-trash-can" @click.prevent="deleteMenuItem(index)"></button>
+        </template>
+        <template v-if="index === elValue['menu-items'].length - 1">
+          <select name="menuChange" v-model="menuChange">
+            <option value="" disabled selected>Change Menu</option>
+            <option value="color">Text Color</option>
+            <option value="backgroundColor">Background Color</option>
+            <option value="addItem">Add Menu Item</option>
+            <option value="deleteMenu">Delete Menu</option>
+          </select>
+          <template v-if="menuChange.toLowerCase().includes('color')">
+            <span
+              :style="{
+                color: this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['backgroundColor'],
+              }"
+              style="filter: invert(100%)"
+              >Text Color:</span
+            ><input
+              type="color"
+              :name="'horizontal_menu' + menuChange"
+              v-model="this.site.params.htmlElements[elIndex]['horizontal-menu']['style'][menuChange]"
+          /></template>
+        </template>
+      </template>
       <template v-else
         ><a
           :href="'#' + menuItem"
           :style="{
-            color: this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['color'],
+            color: this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['color']
+              ? this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['color']
+              : '#000000',
           }"
           >{{ menuItem }}</a
         >
         <template v-if="index === elValue['menu-items'].length - 1">
-          <a class="icon" @click.prevent="respToggle = !respToggle"><i class="fa fa-bars"></i></a></template
+          <a class="icon" @click.prevent="respToggle = !respToggle"
+            ><i
+              class="fa fa-bars"
+              :style="{
+                color: this.site.params.htmlElements[elIndex]['horizontal-menu']['style']['backgroundColor'],
+              }"
+              style="filter: invert(100%)"
+            ></i></a></template
       ></template>
     </template>
   </div>
 </template>
 
 <script>
+// style="color: #ffffff"
 export default {
   name: 'Horizontal_menu',
 
@@ -52,18 +81,73 @@ export default {
   },
 
   data() {
-    return { respToggle: true };
+    return { respToggle: true, menuChange: '' };
   },
 
-  methods: {},
+  methods: {
+    deleteMenuItem(menuItemIndex) {
+      this.site.params.htmlElements[this.elIndex]['horizontal-menu']['menu-items'].splice(menuItemIndex, 1);
+    },
+  },
 };
 </script>
 
 <style>
-.horizontal-menu input {
-  margin: 7px 8px;
+/* span {
+  cursor: pointer;
+} */
+/* .number {
+  margin: 100px;
+} */
+.minus {
+  margin-left: 10px;
+  margin-right: -10px;
+}
+.plus {
+  margin-left: -10px;
+  margin-right: 10px;
+}
+.minus,
+.plus {
+  cursor: pointer;
+  width: 10px;
+  /* height: 0px; */
+  background: #ffffff;
+  /* border-radius: 4px; */
+  padding: 4.5px;
+  border: 1px solid #000000;
+  display: inline-block;
+  vertical-align: middle;
+  text-align: center;
+}
+
+/* input {
+  height: 34px;
+  width: 100px;
+  text-align: center;
+  font-size: 26px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  display: inline-block;
+  vertical-align: middle;
+} */
+
+.horizontal-menu select {
+  margin: 7px 8px 7px 16px;
   padding: 5px;
-  width: 40px;
+}
+
+.horizontal-menu input[type='text'] {
+  margin: 7px -1px 7px 8px;
+  padding: 5px;
+  /* width: auto; */
+  /* height: 20px; */
+  text-align: left;
+}
+
+.horizontal-menu button {
+  margin: 7px 7px 7px 0px;
+  padding: 6px;
 }
 
 .horizontal-menu span {
