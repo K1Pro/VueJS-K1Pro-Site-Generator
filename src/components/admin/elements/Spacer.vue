0da88d150e588d162b7d4bd8d620f2bfc43b41d1
K1Pro <support@k1pro.net>
2024-09-18 20:16:44 +0200

    improved spacing for spacer

--- src/components/admin/elements/Spacer.vue
+++ src/components/admin/elements/Spacer.vue
@@ -1,6 +1,8 @@
 <template>
   <div :id="elKey" class="spacer" :style="[style.outline.borderColor]">
-    <span class="dim" :style="[style.outline.color]">{{ elValue.style.height }}vh</span>
+    <span class="dim" :style="[style.outline.color]"
+      >{{ elValue.style.height >= 1 ? elValue.style.height : '1' }}vh</span
+    >
     <p :style="[spacerHeight]" ref="spacerP"></p>
     <div @mousedown="startResizeSpacer" @mouseup="stopResizeSpacer"></div>
   </div>
@@ -10,13 +12,13 @@
 export default {
   name: 'Spacer',
 
-  inject: ['grid', 'page', 'site', 'style'],
+  inject: ['grid', 'page', 'pageElTypes', 'site', 'style'],
 
   props: ['elKey', 'elValue', 'elIndex'],
 
   computed: {
     spacerHeight() {
-      return { height: this.elValue.style.height + 'vh' };
+      return { height: this.elValue.style.height >= 1 ? this.elValue.style.height + 'vh' : '1vh' };
     },
   },
 
@@ -25,32 +27,48 @@ export default {
       mouseYCoord: null,
       newSpacerHeight: null,
       startingSpacerHeight: this.site.htmlElmnts[this.elKey].style.height,
+      modifyingSpacerHeight: false,
     };
   },
   methods: {
     resizeSpacer(event) {
       if (this.mouseYCoord === null) this.mouseYCoord = event.clientY / this.grid.hght;
       const newSpacerHeight = -100 * (this.mouseYCoord - event.clientY / this.grid.hght);
-      if (window.innerHeight > event.clientY && this.startingSpacerHeight + newSpacerHeight > 1) {
-        this.site.htmlElmnts[this.elKey].style.height =
-          Math.round((this.startingSpacerHeight + newSpacerHeight) * 100) / 100;
+
+      if (window.innerHeight > event.clientY) {
+        if ((this.$refs.spacerP.clientHeight / this.grid.hght) * 100 >= 1) {
+          const numberOfSpacers = this.site.pages[this.page.slctd]
+            .slice(0, this.elIndex + 1)
+            .filter((page) => page[0] == this.elKey && page[1] == true).length;
+          this.site.htmlElmnts[this.elKey].style.height =
+            Math.round((this.startingSpacerHeight + newSpacerHeight / numberOfSpacers) * 100) / 100;
+        } else {
+          this.site.htmlElmnts[this.elKey].style.height = 1;
+        }
       }
     },
     startResizeSpacer() {
       document.body.classList.add('prevent-select');
       document.body.classList.add('ns-resize');
+      this.modifyingSpacerHeight = true;
       document.addEventListener('mousemove', this.resizeSpacer, true);
       document.addEventListener('mouseup', this.stopResizeSpacer, true);
     },
     stopResizeSpacer() {
       this.patchUserSettings ? this.patchUserSettings(this.userSettings) : false;
+      this.modifyingSpacerHeight = false;
       this.mouseYCoord = null;
       this.newSpacerHeight = null;
-      this.startingSpacerHeight = this.site.htmlElmnts[this.elKey].style.height;
+      this.startingSpacerHeight = this.elValue.style.height >= 1 ? this.site.htmlElmnts[this.elKey].style.height : 1;
       document.removeEventListener('mousemove', this.resizeSpacer, true);
       document.removeEventListener('mouseup', this.stopResizeSpacer, true);
       document.body.classList.remove('ns-resize');
       document.body.classList.remove('prevent-select');
+    },
+  },
+  watch: {
+    spacerHeight() {
+      if (!this.modifyingSpacerHeight) this.startingSpacerHeight = Number(this.spacerHeight.height.replace('vh', ''));
     },
   },
 };
