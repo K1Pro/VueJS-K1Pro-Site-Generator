dc5b210f7f79792dcb155c9a8dd2e5412a91428c
K1Pro <support@k1pro.net>
2025-02-05 22:50:14 +0100

    refactoring external links

--- src/components/admin/Website.vue
+++ src/components/admin/Website.vue
@@ -15,12 +15,49 @@
             .trim()
         }}
       </option>
+      <option value="external_links">external links</option>
     </select>
     <p v-if="pageStyle.toLowerCase().includes('color')">
       <input type="color" v-model="site.body.style[pageStyle]" @input="changeColor($event.target.value, pageStyle)" />
     </p>
     <p v-else-if="pageStyle.toLowerCase().includes('opacity') || pageStyle.toLowerCase().includes('margin')">
       <input type="range" v-model="site.body.style[pageStyle]" />{{ site.body.style[pageStyle] }}%
+    </p>
+    <p v-else-if="pageStyle == 'external_links'">
+      <select
+        style="width: calc(100% - 25px); height: 25px"
+        ref="selectLinkTitle"
+        @change="
+          $event.target.nextSibling.value = $event.target.value != 'createExtrnlLink' ? $event.target.value : '';
+          $event.target.nextSibling.nextSibling.nextSibling.value =
+            $event.target.value != 'createExtrnlLink' ? site.links[$event.target.value] : '';
+        "
+      >
+        <option value="createExtrnlLink">Create link</option>
+        <option v-if="Object.keys(site.links).length > 0" disabled selected>Select a link</option>
+        <option v-for="link in Object.keys(site.links)" :value="link">{{ link }}</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Enter new link or select existing"
+        style="
+          padding-left: 5px;
+          width: calc(100% - 50px);
+          margin-left: calc(-99% + 25px);
+          margin-right: 25px;
+          height: 21px;
+          border: none;
+          outline: none;
+        "
+        @change="addLink"
+        ref="linkTitle"
+      />
+      <i
+        class="fa-solid fa-circle-minus"
+        style="color: red; cursor: pointer"
+        @click="deleteLink($event.target.previousSibling.previousSibling.value)"
+      ></i>
+      <input type="text" placeholder="Link URL" style="padding-left: 5px" @change="addLink" ref="linkURL" />
     </p>
     <p v-else-if="pageStyle != ''"><select v-model="site.body.style[pageStyle]"></select></p>
   </div>
@@ -48,6 +85,31 @@ export default {
         allElmnts.forEach((el) => {
           el.style.color = event;
         });
+      }
+    },
+    addLink() {
+      if (this.$refs.linkURL.value && this.$refs.linkTitle.value) {
+        if (this.site.links === null) this.site.links = {};
+        if (this.$refs.selectLinkTitle.value != 'null') {
+          if (this.$refs.selectLinkTitle.value == 'createExtrnlLink') {
+            this.site.links[this.$refs.linkTitle.value] = this.$refs.linkURL.value;
+          } else if (this.$refs.linkTitle.value != this.$refs.selectLinkTitle.value) {
+            this.site.links[this.$refs.linkTitle.value] = this.site.links[this.$refs.selectLinkTitle.value];
+            delete this.site.links[this.$refs.selectLinkTitle.value];
+          } else {
+            this.site.links[this.$refs.selectLinkTitle.value] = this.$refs.linkURL.value;
+          }
+        } else {
+          this.site.links[this.$refs.linkTitle.value] = this.$refs.linkURL.value;
+        }
+      }
+    },
+    deleteLink(event) {
+      if (confirm('Are you sure you would like to delete this link?') == true) {
+        delete this.site.links[event];
+        this.$refs.selectLinkTitle.value = 'createExtrnlLink';
+        this.$refs.linkTitle.value = '';
+        this.$refs.linkURL.value = '';
       }
     },
   },
